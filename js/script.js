38a43f02c1341b0de9a4d3ffcaecc1508698bec8
Sebastián M <smunozmo@gmail.com>
2021-07-05 15:24:47 -0500

    Store input value with localstorage.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@
 const bookTitle = document.getElementById("title");
 const bookAuthor = document.getElementById("author");
 const bookForm = document.getElementById("form");
+const bookAddBtn = document.getElementById('add');
+const bookArray = [];
+let index = 0;
 
 function bookUpdate() {
   const bookList = {
@@ -10,12 +13,22 @@ function bookUpdate() {
     author: bookAuthor.value,
   };
 
+  
   localStorage.setItem("bookList", JSON.stringify(bookList));
+
+  bookArray.push(bookList);
+  index += 1;
+
+  localStorage.setItem("bookObject", JSON.stringify(bookArray));
+
+  const bookArrayBook = JSON.parse(localStorage.getItem('bookObject'))
+
+  console.log(bookArray);
 }
 
-bookForm.addEventListener("input", bookUpdate);
+bookAddBtn.addEventListener("click", bookUpdate);
 
-console.log("hi");
+
 
 // List creation
 const bookContainer = document.getElementById("list");
@@ -29,10 +42,10 @@ function showBook(e) {
   `
 <li class="list-group-item d-flex justify-content-between align-items-start list-group-item-action">
 <div class="ms-2 me-auto">
-    <h2 class="fw-bold">${bookArray[e].title}</h2>
-    <h3>${bookArray[e].author}</h3>
+    <h2 class="fw-bold">${bookArrayObject[e].title}</h2>
+    <h3>${bookArrayObject[e].author}</h3>
 </div>
-<button type="button" class="btn btn-warning"> <i class="far fa-trash-alt"></i> </button>
+<button type="button" class="btn btn-warning" id="book-id-${e}"> <i class="far fa-trash-alt"></i> </button>
 </li>
 `;
 }
