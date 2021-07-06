311cebb097bfa15dead2df7acf8bf530492b16d0
Sebastián M <smunozmo@gmail.com>
2021-07-06 11:56:54 -0500

    Fix linter error.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 // Localstorage Books
 
-const bookTitle = document.getElementById("title");
-const bookAuthor = document.getElementById("author");
-const bookAddBtn = document.getElementById("add");
+const bookTitle = document.getElementById('title');
+const bookAuthor = document.getElementById('author');
+const bookAddBtn = document.getElementById('add');
 
 // List creation
-const bookContainer = document.getElementById("list");
-const bookContainerUl = document.createElement("ul");
-bookContainerUl.className = "list-group list-group-numbered";
+const bookContainer = document.getElementById('list');
+const bookContainerUl = document.createElement('ul');
+bookContainerUl.className = 'list-group list-group-numbered';
 bookContainer.appendChild(bookContainerUl);
 
 let bookArray = [];
 
-if (localStorage.getItem("bookObject") !== null) {
-  JSON.parse(localStorage.getItem("bookObject")).forEach((element) => {
+if (localStorage.getItem('bookObject') !== null) {
+  JSON.parse(localStorage.getItem('bookObject')).forEach((element) => {
     bookArray.push(element);
   });
 } else {
@@ -21,10 +21,10 @@ if (localStorage.getItem("bookObject") !== null) {
 }
 
 function showBook() {
-  bookContainerUl.innerHTML = ""; // reset the content to avoid acumulation
+  bookContainerUl.innerHTML = ''; // reset the content to avoid acumulation
   for (let e = 0; e < bookArray.length; e += 1) {
     bookContainerUl.innerHTML += `
-      <li class="list-group-item d-flex justify-content-between align-items-start list-group-item-action p-4" id="${e}">
+      <li class="list-group-item d-flex justify-content-between align-items-start list-group-item-action p-4">
   <div class="ms-2 me-auto">
       <h2 class="fw-bold">${bookArray[e].title}</h2>
       <h3>${bookArray[e].author}</h3>
@@ -43,14 +43,14 @@ function bookUpdate() {
   };
 
   bookArray.push(bookList);
-  localStorage.setItem("bookObject", JSON.stringify(bookArray));
+  localStorage.setItem('bookObject', JSON.stringify(bookArray));
 
   showBook();
 }
 
 function bookRemove(e) {
   bookArray.splice(e, 1);
-  localStorage.setItem("bookObject", JSON.stringify(bookArray));
+  localStorage.setItem('bookObject', JSON.stringify(bookArray));
   showBook();
 }
 
@@ -59,9 +59,9 @@ if (!bookArray) {
   bookRemove(0);
 }
 
-bookAddBtn.addEventListener("click", () => {
-  if (bookTitle.value === "" || bookAuthor.value === "") {
-    bookAddBtn.setCustomValidity("Oops! Please type an author and title.");
+bookAddBtn.addEventListener('click', () => {
+  if (bookTitle.value === '' || bookAuthor.value === '') {
+    bookAddBtn.setCustomValidity('Oops! Please type an author and title.');
   } else {
     bookUpdate();
   }
