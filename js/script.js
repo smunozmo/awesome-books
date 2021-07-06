ffd654173dd31340b7acb6fbd386f6ab35b7b071
afcastaneda223 <af.castaneda223@gmail.com>
2021-07-06 18:27:24 -0300

    Add class for book and library controller for action

--- js/script.js
+++ js/script.js
@@ -1,76 +1,65 @@
 // Localstorage Books
 
-const bookTitle = document.getElementById('title');
-const bookAuthor = document.getElementById('author');
-const bookAddBtn = document.getElementById('add');
+const bookTitle = document.getElementById("title");
+const bookAuthor = document.getElementById("author");
+const bookAddBtn = document.getElementById("add");
 
 // List creation
-const bookContainer = document.getElementById('list');
-const bookContainerUl = document.createElement('ul');
-bookContainerUl.className = 'list-group list-group-numbered';
+const bookContainer = document.getElementById("list");
+const bookContainerUl = document.createElement("ul");
+bookContainerUl.className = "list-group list-group-numbered";
 bookContainer.appendChild(bookContainerUl);
 
-
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
 let bookArray = new Array();
 
-if (localStorage.getItem('bookObject') !== null) {
-  JSON.parse(localStorage.getItem('bookObject')).forEach((element) => {
+if (localStorage.getItem("bookObject") !== null) {
+  JSON.parse(localStorage.getItem("bookObject")).forEach((element) => {
     bookArray.push(element);
   });
 } else {
   bookArray = [];
 }
 
-function showBook() {
-  bookContainerUl.innerHTML = ''; // reset the content to avoid acumulation
-  for (let e = 0; e < bookArray.length; e += 1) {
-    bookContainerUl.innerHTML += `
-      <li class="list-group-item d-flex justify-content-between align-items-start list-group-item-action p-4">
-  <div class="ms-2 me-auto">
-      <h2 class="fw-bold">${bookArray[e].title}</h2>
-      <h3>${bookArray[e].author}</h3>
-  </div>
-  <button type="button" class="btn btn-warning fs-2" onclick="bookRemove(${e})"> <i class="far fa-trash-alt fs-2"></i> </button>
-  </li>`;
+class Library {
+  showBook(bookArray) {
+    bookContainerUl.innerHTML = ""; // reset the content to avoid acumulation
+    for (let e = 0; e < bookArray.length; e += 1) {
+      bookContainerUl.innerHTML += `
+    <li class="list-group-item d-flex justify-content-between align-items-start list-group-item-action p-4">
+    <div class="ms-2 me-auto">
+    <h2 class="fw-bold">${bookArray[e].title}</h2>
+    <h3>${bookArray[e].author}</h3>
+    </div>
+    <button type="button" class="btn btn-warning fs-2" onclick="a.bookRemove(${e})"> <i class="far fa-trash-alt fs-2"></i> </button>
+    </li>`;
+    }
+  }
+
+  bookUpdate(x) {
+    bookArray.push(x);
+    localStorage.setItem("bookObject", JSON.stringify(bookArray));
+    return bookArray;
+  }
+
+  bookRemove(e) {
+    bookArray.splice(e, 1);
+    localStorage.setItem("bookObject", JSON.stringify(bookArray));
   }
 }
 
-showBook(); // run the function to show the list on load
-
-class Books {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
-};
-
-function bookUpdate() {
-
-  let bookList = new Books(bookTitle.value, bookAuthor.value);
-
-  
-
-  bookArray.push(bookList);
-  localStorage.setItem('bookObject', JSON.stringify(bookArray));
-
-  showBook();
-}
-
-function bookRemove(e) {
-  bookArray.splice(e, 1);
-  localStorage.setItem('bookObject', JSON.stringify(bookArray));
-  showBook();
-}
-
-if (!bookArray) {
-  // Avoid linter error
-  bookRemove(0);
-}
-
-bookAddBtn.addEventListener('click', () => {
-  if (bookTitle.value === '' || bookAuthor.value === '') {
-    bookAddBtn.setCustomValidity('Oops! Please type an author and title.');
+bookAddBtn.addEventListener("click", () => {
+  let a = new Library();
+  if (bookTitle.value === "" || bookAuthor.value === "") {
+    bookAddBtn.setCustomValidity("Oops! Please type an author and title.");
   } else {
-    bookUpdate();
+    bookList = new Book(bookTitle.value, bookAuthor.value);
+    a.bookUpdate(bookList);
+    a.showBook(bookArray);
   }
 });
