1f12b82b15c6b10e62009cd559917cc5aef72ea5
Sebastián M <smunozmo@gmail.com>
2021-07-05 19:05:12 -0500

    Add comments on script.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,14 @@ const bookAuthor = document.getElementById("author");
 const bookForm = document.getElementById("form");
 const bookAddBtn = document.getElementById("add");
 
+// List creation
+const bookContainer = document.getElementById("list");
+
+const bookContainerUl = document.createElement("ul");
+bookContainerUl.className = "list-group list-group-numbered";
+
+bookContainer.appendChild(bookContainerUl);
+
 
 // let bookArray = [JSON.parse(localStorage.getItem("bookObject"))];
 let bookArray = [];
@@ -18,6 +26,23 @@ JSON.parse(localStorage.getItem("bookObject")).forEach(element => {
  } else {
  bookArray = [];
 }
+
+showBook(); // run the function to show the list on load
+
+function showBook() {
+  bookContainerUl.innerHTML = ''; // reset the content to avoid acumulation
+for (let e = 0; e < bookArray.length; e++) {
+  bookContainerUl.innerHTML += `
+  <li class="list-group-item d-flex justify-content-between align-items-start list-group-item-action">
+  <div class="ms-2 me-auto">
+      <h2 class="fw-bold">${bookArray[e].title}</h2>
+      <h3>${bookArray[e].author}</h3>
+  </div>
+  <button type="button" class="btn btn-warning" id="book-id-${e}"> <i class="far fa-trash-alt"></i> </button>
+  </li>`
+  }
+
+ }
 
 function bookUpdate() {
   const bookList = {
@@ -33,35 +58,14 @@ function bookUpdate() {
   localStorage.setItem("bookObject", JSON.stringify(bookArray));
   
  
-  
-function showBook(e) {
-  return `
-  <li class="list-group-item d-flex justify-content-between align-items-start list-group-item-action">
-  <div class="ms-2 me-auto">
-      <h2 class="fw-bold">${bookArray[e].title}</h2>
-      <h3>${bookArray[e].author}</h3>
-  </div>
-  <button type="button" class="btn btn-warning" id="book-id-${e}"> <i class="far fa-trash-alt"></i> </button>
-  </li>`;
-} 
+  showBook();
 
-for (let index = 0; index < bookArray.length; index++) {
-  bookContainerUl.innerHTML += showBook(index);
 }
-
- }
 
 bookAddBtn.addEventListener("click", bookUpdate);
 
 
 
-// List creation
-const bookContainer = document.getElementById("list");
-
-const bookContainerUl = document.createElement("ul");
-bookContainerUl.className = "list-group list-group-numbered";
-
-bookContainer.appendChild(bookContainerUl);
 
 
 
